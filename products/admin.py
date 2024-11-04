02ef15e57bfa846fe7b6c3fd9e93ace64c825f86
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-11-04 20:46:16 -0300

    Add initial product and category models with admin registration and basic views

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
 
 # Register your models here.
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
