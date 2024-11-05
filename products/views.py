7e543d6d67c65aa68b36035f394524e320c8bb1f
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-11-05 10:40:59 -0300

    Add product detail view and URL route for individual product display

--- products/views.py
+++ products/views.py
@@ -8,3 +8,9 @@ def index(request):
     products = Product.objects.all()
 
     return render(request, "index.html", {"products": products})
+
+
+def detalle(request, producto_id):
+    product = Product.objects.get(id=producto_id)
+
+    return render(request, "detalle.html", {"product": product})
