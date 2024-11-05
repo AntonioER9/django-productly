791ae7d78ae92b3e441a2a2236461f5bd681543e
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-11-05 10:38:12 -0300

    Update products index view to render HTML template with product list

--- products/views.py
+++ products/views.py
@@ -5,8 +5,6 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, world. You're at the products index.")
     products = Product.objects.all()
-    # products = Product.objects.filter(is_active=True)
-    # return HttpResponse(products)
-    return JsonResponse(list(products.values()), safe=False)
+
+    return render(request, "index.html", {"products": products})
