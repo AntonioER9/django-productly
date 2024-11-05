c94fd57391b5a4617531491cb969e7273521e566
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-11-05 10:30:04 -0300

    Update products index view to return JSON response with all products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the products index.")
+    # return HttpResponse("Hello, world. You're at the products index.")
+    products = Product.objects.all()
+    # products = Product.objects.filter(is_active=True)
+    # return HttpResponse(products)
+    return JsonResponse(list(products.values()), safe=False)
